3a32c2420a699e54c435e5588e7cff218cfe6443
Lucas <lucasprograms@gmail.com>
2021-03-21 12:39:28 -0400

    Update header-link.js

--- src/components/header/header-link/header-link.js
+++ src/components/header/header-link/header-link.js
@@ -5,8 +5,8 @@ import { Link } from 'gatsby'
 const Header = ({ title, to }) => (
   <Link
     to={to}
-    className="font-sans text-lg leading-5 no-underline mr-5 border-b-4 border-orange border-opacity-0 hover:border-opacity-100"
-    activeClassName="border-opacity-100"
+    className="font-sans text-lg leading-5 no-underline mr-5 border-b-4 border-opacity-5 lg:border-orange lg:border-opacity-0 hover:border-opacity-100"
+    activeClassName="border-opacity-100 lg:border-opacity-100"
   >
     {title}
   </Link>
