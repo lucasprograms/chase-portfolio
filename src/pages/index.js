39bb34286906f52fd67f799b8ce79e544731464a
Lucas <lucasprograms@gmail.com>
2021-03-20 23:46:04 -0400

    index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,31 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react'
+import { Link } from 'gatsby'
+import BlobHero from '../images/svg/blob-hero.inline.svg'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <section
+      className="grid grid-cols-1 lg:grid-cols-2"
+      style={{ height: '920px' }}
+    >
+      <div class="flex flex-col justify-center z-10">
+        <h1 className="text-4xl font-serif max-w-md self-center">
+          Hey, I’m Chase—I’m a UX Designer and cat enthusiast.{' '}
+        </h1>
+        <p className="text-2xl max-w-md self-center">
+          I love listening to and exploring people’s experiences, and using what
+          I learn to create products that make them happy.
+        </p>
+      </div>
+      <div className="absolute lg:static top-0">
+        {' '}
+        <BlobHero className="absolute top-0" />
+      </div>
+    </section>
   </Layout>
 )
 
