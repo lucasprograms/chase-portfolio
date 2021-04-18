17ddd35beb9f0c15e34099062e00c1cb28cc5be1
Lucas <lucasprograms@gmail.com>
2021-04-18 10:06:18 -0400

    pgs research

--- src/components/case-study/pgs/research.js
+++ src/components/case-study/pgs/research.js
@@ -19,23 +19,29 @@ const Define = ({ title, highlightColor }) => {
             focus my redesign efforts. Here were my main takeaways:
           </p>
         </div>
-        <div className="grid lg:grid-cols-3 lg:col-span-6 mt-20">
-          <div className="lg:col-span-2 flex items-center mt-20">
-            <CircleSeventyOne />
-            <span className="text-2xl ml-5">
+        <div className="grid lg:grid-cols-6 lg:col-span-6 lg:mt-20">
+          <div className="lg:col-span-3 flex flex-col lg:flex-row items-center mt-20">
+            <div style={{ height: '160px', width: '160px' }}>
+              <CircleSeventyOne />
+            </div>
+            <span className="text-2xl ml-0 lg:ml-5 mt-5 lg:mt-0 text-center lg:text-left">
               The site had a 71% bounce rate.
             </span>
           </div>
-          <div className="lg:col-span-2 lg:col-start-2 flex items-center mt-20">
-            <CircleSeventySix />
-            <span className="text-2xl ml-5">
+          <div className="lg:col-span-3 lg:col-start-3 flex flex-col lg:flex-row items-center mt-20">
+            <div style={{ height: '160px', width: '160px' }}>
+              <CircleSeventySix />
+            </div>
+            <span className="text-2xl ml-0 lg:ml-5 mt-5 lg:mt-0 text-center lg:text-left">
               On top of that, 76% of the site’s session over the last calendar
               year had lasted 10 seconds or less.
             </span>
           </div>
-          <div className="lg:col-span-2 flex items-center mt-20">
-            <CircleFifty />
-            <span className="text-2xl ml-5">
+          <div className="lg:col-span-3 flex flex-col lg:flex-row items-center mt-20">
+            <div style={{ height: '160px', width: '160px' }}>
+              <CircleFifty />
+            </div>
+            <span className="text-2xl ml-0 lg:ml-5 mt-5 lg:mt-0 text-center lg:text-left">
               The homepage, the entry page for more than half of the site’s
               visitors, also had an exit rate of 50%.
             </span>
